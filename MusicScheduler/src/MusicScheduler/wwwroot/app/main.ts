9cf1ceda97846bc234900e9465a9147e5697a6d0
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2016-05-17 21:04:07 +0200

    Basic angular.io application is now working

--- MusicScheduler/src/MusicScheduler/wwwroot/app/main.ts
+++ MusicScheduler/src/MusicScheduler/wwwroot/app/main.ts
@@ -1,2 +1,11 @@
-﻿import {bootstrap} from 'angular2/platform/browser'
+﻿import {Component} from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
 
+@Component({
+    selector: 'MusicSchedulerApp',
+    template: '<h2>It works!</h2>'
+})
+
+export class App {
+
+}
